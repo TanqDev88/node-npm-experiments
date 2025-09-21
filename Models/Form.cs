6c066b521359d9f756bd9cd1b8f111c1795bb4d4
TanqDev88 <oliverprim783@gmail.com>
2025-09-21 20:01:44 -0300

    refactor: remove old structure and create new project base

--- Models/Form.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class Form
-{
-    [Required ]
-    [Display (Name = "Correos")]
-    public string Mail { get; set; }
-    public string Password { get; set; }
-}

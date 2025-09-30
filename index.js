8896a36efd735ad4477f4ce691418454c13e4c7b
TanqDev88 <oliverprim783@gmail.com>
2025-09-29 23:53:23 -0300

    feat: .env file is added

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log(Math.random())
+
+let misValores = 'cire de node';
+misValores = 10;
+
+console.log(misValores);
+
+//tipado dinamico en JS: En la linea 3 se define y en la 4 se redefine el valor de misValores
+
+const lista = "uno, dos, tres"
+
+const listaArray = ['uno', 'dos', 'tres']
+
+console.log(lista);
+
+console.log(listaDos);
+
+console.log(listaDos[0]);
+
+//Snippet console.log = clg
+
+let nodeNode = {
+    tema: "Node lenguaje",
+    curso: "Node basico"
+}
+
+console.log(nodeNode);//Se imprime todo el objeto
+console.log(nodeNode.tema);//Se accede a la propiedad tema

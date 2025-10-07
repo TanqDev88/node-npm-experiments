c092c41255641b0bc4500772393e963362561328
TanqDev88 <oliverprim783@gmail.com>
2025-10-06 23:59:18 -0300

    feat:modifications to the app.js file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import{createServer} from 'node:http';
 
-const servidor = createServer ( (solicitud, respuesta)=>{
+const servidor = createServer ( (peticion, respuesta)=>{
 console.log('Respuesta de que alguien intento entrar en el server');
+
+
+
+respuesta.writeHead(200, {'content-type': 'text/html'});
+respuesta.write('respuesta parar la url', peticion.url);
 })
 
 servidor.listen(3000);

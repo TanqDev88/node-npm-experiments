9e5b3063b1cfe1b66579dc8ddee4c0fec1c59781
TanqDev88 <oliverprim783@gmail.com>
2025-10-08 23:38:28 -0300

    fix: correct string concatenation in HTTP server response

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log('Respuesta de que alguien intento entrar en el server');
 
 
 respuesta.writeHead(200, {'content-type': 'text/html'});
-respuesta.write('respuesta parar la url', peticion.url);
+respuesta.write('respuesta parar la url'+ peticion.url);
 })
 
 servidor.listen(3000);
